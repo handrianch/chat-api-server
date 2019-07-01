790bc8d42dc03034f0efc3d174883d410a8c6d78
candra handrian <handrian.ch@gmal.com>
2019-07-02 01:31:12 +0700

    add migration and run

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -6,10 +6,9 @@ const Schema = use('Schema')
 class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
+      table.uuid('id').primary()
+      table.string('username', 30).notNullable().unique()
+      table.string('password', 255).notNullable()
       table.timestamps()
     })
   }
