341ea7e5a3af331bcadd2c69598ec2edc157a263
candra handrian <handrian.ch@gmal.com>
2019-07-07 22:52:49 +0700

    fix response data from api /rooms/:id/chats

--- app/Controllers/Http/v2/UserRoomController.js
+++ app/Controllers/Http/v2/UserRoomController.js
@@ -19,21 +19,20 @@ class UserRoomController {
     const user = await auth.getUser()
     const page = request.get('page').page || 1
     const limit = Env.get('PAGE_LIMIT')
+    const roomId = params.id
 
     /*
       SELECT chats.*, users.username, users.name FROM chats INNER JOIN user_room ON chats.user_room_id = user_room.id INNER JOIN users ON user_room.user_id = users.id WHERE user_room.room_id = 1 ORDER BY chats.created_at ASC;
     */
 
-    // const results = Database.select('chats.*, users.username, users.name')
-    //                         .from('chats')
-    //                         .innerJoin('user_room', 'chats.user_room_id', 'user_room.id')
-    //                         .innerJoin('users', 'user_room.user_id', 'users.id')
-    //                         .where('user_room.room_id', 1)
-    //                         .orderBy('chats.created_at', 'ASC')
-    // console.log(results)
-    const results = await Database.raw("SELECT chats.*, users.username, users.name FROM chats INNER JOIN user_room ON chats.user_room_id = user_room.id INNER JOIN users ON user_room.user_id = users.id WHERE user_room.room_id = ? ORDER BY chats.created_at ASC", params.id)
+    const results = await Database.select('chats.*', 'users.username', 'users.name')
+                                  .from('chats')
+                                  .innerJoin('user_room', 'chats.user_room_id', 'user_room.id')
+                                  .innerJoin('users', 'user_room.user_id', 'users.id')
+                                  .where('user_room.room_id', roomId)
+                                  .orderBy('chats.created_at', 'ASC')
     return response.status(200)
-                   .send(results.toJson())
+                   .send(results)
   }
 
   async showMembers ({request, response, params, auth}) {
